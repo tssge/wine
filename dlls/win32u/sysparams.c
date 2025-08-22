tssge/wine

commit 31fda1f4a08e61daa3c5e84829f80dd3ed948d8a

win32u: Support windows spanning multiple monitors in map_window_rects_virt_to_raw().
Fix a regression from 545073fd, which restricts the visible rect to only one monitor when the
original virtual visible rect is spanning multiple monitors.

Fix Project CARS 3 (958400) fails to enter triple screen mode.

dlls/win32u/sysparams.c
@@ -2601,22 +2601,29 @@ RECT map_rect_virt_to_raw( RECT rect, UINT dpi_from )
 /* map (absolute) window rects from MDT_DEFAULT to MDT_RAW_DPI coordinates */
 struct window_rects map_window_rects_virt_to_raw( struct window_rects rects, UINT dpi_from )
 {
+    RECT rect, monitor_rect, virt_visible_rect = rects.visible;
     struct monitor *monitor;
-    RECT rect, monitor_rect;
     BOOL is_fullscreen;
 
     if (!lock_display_devices( FALSE )) return rects;
     if ((monitor = get_monitor_from_rect( rects.window, MONITOR_DEFAULTTONEAREST, dpi_from, MDT_DEFAULT )))
     {
-        /* if the visible rect is fullscreen, make it cover the full raw monitor, regardless of aspect ratio */
-        monitor_rect = monitor_get_rect( monitor, dpi_from, MDT_DEFAULT );
-
-        is_fullscreen = intersect_rect( &rect, &monitor_rect, &rects.visible ) && EqualRect( &rect, &monitor_rect );
-        if (is_fullscreen) rects.visible = monitor_get_rect( monitor, 0, MDT_RAW_DPI );
-        else rects.visible = map_monitor_rect( monitor, rects.visible, dpi_from, MDT_DEFAULT, 0, MDT_RAW_DPI );
-
+        rects.visible = map_monitor_rect( monitor, rects.visible, dpi_from, MDT_DEFAULT, 0, MDT_RAW_DPI );
         rects.window = map_monitor_rect( monitor, rects.window, dpi_from, MDT_DEFAULT, 0, MDT_RAW_DPI );
         rects.client = map_monitor_rect( monitor, rects.client, dpi_from, MDT_DEFAULT, 0, MDT_RAW_DPI );
+    }
+    /* if the visible rect is fullscreen, make it cover the full raw monitor, regardless of aspect ratio */
+    LIST_FOR_EACH_ENTRY(monitor, &monitors, struct monitor, entry)
+    {
+        if (!is_monitor_active( monitor ) || monitor->is_clone) continue;
+
+        monitor_rect = monitor_get_rect( monitor, dpi_from, MDT_DEFAULT );
+        is_fullscreen = intersect_rect( &rect, &monitor_rect, &virt_visible_rect ) && EqualRect( &rect, &monitor_rect );
+        if (is_fullscreen)
+        {
+            rect = monitor_get_rect( monitor, 0, MDT_RAW_DPI );
+            union_rect( &rects.visible, &rects.visible, &rect );
+        }
     }
     unlock_display_devices();
 

commit f2b08298382a2b463d377dd7c1e33bfa6f060d3d

win32u: Limit fullscreen visible rects to the monitor rects.
Fix a regression from 31fda1f4, which allowed the visible rect to be larger than the monitor rect.
After 31fda1f4, CHRONO TRIGGER (613830) sets a window rect slightly larger than the monitor rect and
will change the window rect to the rect it previously set if the game detects a different window rect.
Adding __NET_WM_STATE_FULLSCREEN will cause WMs to move the window to cover exactly the monitor rect.
So the window rect will be repeatedly changed by the WM and the game, causing a flickering effect.
Limit fullscreen visible rects to the monitor rects so that adding __NET_WM_STATE_FULLSCREEN in
winex11.drv won't trigger a size change.

CW-Bug-Id: #25747

dlls/win32u/sysparams.c
@@ -2767,8 +2767,8 @@ RECT map_rect_virt_to_raw_for_monitor( HMONITOR handle, RECT rect, UINT dpi_from
 struct window_rects map_window_rects_virt_to_raw( struct window_rects rects, UINT dpi_from )
 {
     RECT rect, monitor_rect, virt_visible_rect = rects.visible;
+    BOOL is_fullscreen, first_fullscreen = TRUE;
     struct monitor *monitor;
-    BOOL is_fullscreen;
 
     if (!lock_display_devices( FALSE )) return rects;
     if ((monitor = get_monitor_from_rect( rects.window, MONITOR_DEFAULTTONEAREST, dpi_from, MDT_DEFAULT )))
@@ -2786,6 +2786,18 @@ struct window_rects map_window_rects_virt_to_raw( struct window_rects rects, UIN
         is_fullscreen = intersect_rect( &rect, &monitor_rect, &virt_visible_rect ) && EqualRect( &rect, &monitor_rect );
         if (is_fullscreen)
         {
+            /* If the visible rect is fullscreen on any one of the monitors, limit the visible rect
+             * to the monitor rects. This is needed because adding __NET_WM_STATE_FULLSCREEN will
+             * make WMs move the window to cover exactly the monitor rect. If the application sets a
+             * visible rect slightly larger than the monitor rect and insists on changing to the rect
+             * that it previously set when the rect is changed by the WM, then the window rect will
+             * be repeatedly changed by :wthe WM and the application, causing a flickering effect */
+            if (first_fullscreen)
+            {
+                SetRectEmpty( &rects.visible );
+                first_fullscreen = FALSE;
+            }
+
             rect = monitor_get_rect( monitor, 0, MDT_RAW_DPI );
             union_rect( &rects.visible, &rects.visible, &rect );
         }